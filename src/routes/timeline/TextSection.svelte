d7d0d4b6825203eaab35ac866f2706194fe826bf
KaiMagnusMueller <ndsignin@outlook.de>
2022-09-30 19:21:26 +0200

    Reduced text section vertical margin
    Fixed scrollbar appearing on 1970 cotton text

--- src/routes/timeline/TextSection.svelte
+++ src/routes/timeline/TextSection.svelte
@@ -2,7 +2,7 @@
     import { fly } from 'svelte/transition';
 </script>
 
-<div class="text-section margin-v-xl" in:fly={{ delay: 0, duration: 500, x: 25 }}>
+<div class="text-section margin-v-m" in:fly={{ delay: 0, duration: 500, x: 25 }}>
     <slot />
 </div>
 
