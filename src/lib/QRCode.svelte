a9ccd95e6d38cb3445a1424c446953e119728c77
Kai Magnus Müller <ndsignin@outlook.de>
2022-10-02 09:58:41 +0200

    Adde videos, qr codes,
    - Added poster images for videos

---
+++ src/lib/QRCode.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+import Image from "./Image.svelte";
+    export let QR_Code: any
+    export let url: string
+    </script>
+
+    <span class="margin-none margin-b-xs">{url}</span>
+    <div class="qr-code">
+        <Image src={QR_Code}/>
+    </div>
+
+
+<style>
+
+</style>
--- src/routes/timeline/2000/+page.svelte
+++ src/routes/timeline/2000/+page.svelte
@@ -1,115 +1,128 @@
 <script>
-    import { t, locale } from 'svelte-intl-precompile';
-    import { page } from '$app/stores';
-    import { topics, currentYear } from '../store';
-    import Modal from '$lib/Modal.svelte';
-    import TextSection from '../TextSection.svelte';
+    import { t, locale } from "svelte-intl-precompile";
+    import { page } from "$app/stores";
+    import { topics, currentYear } from "../store";
+    import Modal from "$lib/Modal.svelte";
+    import TextSection from "../TextSection.svelte";
 
     // @ts-ignore
-    import Water_de from './Water_de.svx';
+    import Water_de from "./Water_de.svx";
     // @ts-ignore
-    import Water_en from './Water_en.svx';
+    import Water_en from "./Water_en.svx";
     // @ts-ignore
-    import Cotton_de from './Cotton_de.svx';
+    import Cotton_de from "./Cotton_de.svx";
     // @ts-ignore
-    import Cotton_en from './Cotton_en.svx';
+    import Cotton_en from "./Cotton_en.svx";
     // @ts-ignore
-    import Politics_de from './Politics_de.svx';
+    import Politics_de from "./Politics_de.svx";
     // @ts-ignore
-    import Politics_en from './Politics_en.svx';
+    import Politics_en from "./Politics_en.svx";
 
     // @ts-ignore
-    import SeaTable_de from './SeaTable_de.svx';
+    import SeaTable_de from "./SeaTable_de.svx";
     // @ts-ignore
-    import SeaTable_en from './SeaTable_en.svx';
+    import SeaTable_en from "./SeaTable_en.svx";
 
-    import StatsProportions_de from './StatsProportions_de.svelte';
-    import StatsProportions_en from './StatsProportions_en.svelte';
+    import StatsProportions_de from "./StatsProportions_de.svelte";
+    import StatsProportions_en from "./StatsProportions_en.svelte";
 
-    import ImageSection from '../ImageSection.svelte';
-    import Window from '$lib/Window.svelte';
-    import Image from '$lib/Image.svelte';
+    import ImageSection from "../ImageSection.svelte";
+    import Window from "$lib/Window.svelte";
+    import Image from "$lib/Image.svelte";
 
-    import SaigaAntilpoe from '$lib/media/saiga-antilope.jpg';
-    import QuiltProtest from '$lib/media/Quilt-Protest.jpg';
+    import SaigaAntilpoe from "$lib/media/saiga-antilope.jpg";
+    import QuiltProtest from "$lib/media/Quilt-Protest.jpg";
+    import QR_Code from "./QR_Code.svg";
+    import QRCode from "$lib/QRCode.svelte";
 
     $currentYear = 2000;
 
     $topics.topicList = [
         {
-            title: $t('2000.water.title_short'),
-            value: 'water',
+            title: $t("2000.water.title_short"),
+            value: "water",
         },
         {
-            title: $t('2000.cotton.title_short'),
-            value: 'cotton',
+            title: $t("2000.cotton.title_short"),
+            value: "cotton",
         },
         {
-            title: $t('2000.politics.title_short'),
-            value: 'politics',
+            title: $t("2000.politics.title_short"),
+            value: "politics",
         },
     ];
 </script>
 
 <svelte:head>
-    <title>{$t('1960.title')}</title>
-    <meta name="description" content={$t('1960.title')} />
+    <title>{$t("1960.title")}</title>
+    <meta name="description" content={$t("1960.title")} />
 </svelte:head>
 <TextSection>
-    {#if $topics.currentTopic === 'water'}
-        {#if $locale === 'de'}
+    {#if $topics.currentTopic === "water"}
+        {#if $locale === "de"}
             <Water_de />
-        {:else if $locale === 'en'}
+        {:else if $locale === "en"}
             <Water_en />
         {/if}
     {/if}
-    {#if $topics.currentTopic === 'cotton'}
-        {#if $locale === 'de'}
+    {#if $topics.currentTopic === "cotton"}
+        {#if $locale === "de"}
             <Cotton_de />
-        {:else if $locale === 'en'}
+        {:else if $locale === "en"}
             <Cotton_en />
         {/if}
     {/if}
-    {#if $topics.currentTopic === 'politics'}
-        {#if $locale === 'de'}
+    {#if $topics.currentTopic === "politics"}
+        {#if $locale === "de"}
             <Politics_de />
-        {:else if $locale === 'en'}
+        {:else if $locale === "en"}
             <Politics_en />
         {/if}
     {/if}
 </TextSection>
 
 <ImageSection>
-    {#if $topics.currentTopic === 'water'}
-        <div class="second-image from-top-right-corner to-width-80 image image-transition">
+    {#if $topics.currentTopic === "water"}
+        <div
+            class="second-image from-top-right-corner to-width-80 image image-transition"
+        >
             <Window>
-                <Image src={SaigaAntilpoe} alt={'text'} />
+                <Image src={SaigaAntilpoe} alt={"text"} />
             </Window>
         </div>
         <div class="align-bottom margin-l-auto">
-            {#if $locale === 'de'}
+            {#if $locale === "de"}
                 <SeaTable_de />
-            {:else if $locale === 'en'}
+            {:else if $locale === "en"}
                 <SeaTable_en />
             {/if}
         </div>
     {/if}
-    {#if $topics.currentTopic === 'cotton'}
+    {#if $topics.currentTopic === "cotton"}
         <div class="stats center-v-h">
             <Window padding transparent>
-                {#if $locale === 'de'}
+                {#if $locale === "de"}
                     <StatsProportions_de />
-                {:else if $locale === 'en'}
+                {:else if $locale === "en"}
                     <StatsProportions_en />
                 {/if}
             </Window>
         </div>
     {/if}
-    {#if $topics.currentTopic === 'politics'}
-        <div class="karakum-image image image-transition center-v-h">
-            <Window caption={$t('2000.politics.image')}>
-                <Image src={QuiltProtest} alt={$t('2000.politics.image')} />
+    {#if $topics.currentTopic === "politics"}
+        <div class="karakum-image to-width-80 image image-transition ">
+            <Window caption={$t("2000.politics.image")}>
+                <Image src={QuiltProtest} alt={$t("2000.politics.image")} />
             </Window>
+        </div>
+
+        <div
+            class="margin-l-auto flex gap-xxs align-flex-end justify-self-right "
+        >
+            <QRCode
+                url="https://www.youtube.com/watch?v=veNYrJruFLc"
+                {QR_Code}
+            />
         </div>
     {/if}
 </ImageSection>
@@ -122,7 +135,7 @@
 
     .karakum-image {
         position: absolute;
-        width: 1000px;
+        width: 800px;
     }
 
     .stats {
